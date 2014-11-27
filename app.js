740b638cafa9e7d661dbb56deb4838169306c419
Krzysztof Kula <krzychukula@gmail.com>
2014-11-27 10:49:47 +0100

    login?

--- app.js
+++ app.js
@@ -12,13 +12,73 @@ var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var passport = require('passport');
 
-var MongoStore = require('connect-mongo')(session);
-var mongoose = require('mongoose');
+var User = require('./models/User');
+
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 var secrets = {
   db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://localhost:27017/game',
   sessionSecret: process.env.SESSION_SECRET || 'Default session secret',
+  google: {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback',
+    passReqToCallback: true
+  }
 }
+
+
+// Sign in with Google.
+
+passport.use(new GoogleStrategy(secrets.google, function(req, accessToken, refreshToken, profile, done) {
+  if (req.user) {
+    User.findOne({ google: profile.id }, function(err, existingUser) {
+      if (existingUser) {
+        req.flash('errors', { msg: 'There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+        done(err);
+      } else {
+        User.findById(req.user.id, function(err, user) {
+          user.google = profile.id;
+          user.tokens.push({ kind: 'google', accessToken: accessToken });
+          user.profile.name = user.profile.name || profile.displayName;
+          user.profile.gender = user.profile.gender || profile._json.gender;
+          user.profile.picture = user.profile.picture || profile._json.picture;
+          user.save(function(err) {
+            req.flash('info', { msg: 'Google account has been linked.' });
+            done(err, user);
+          });
+        });
+      }
+    });
+  } else {
+    User.findOne({ google: profile.id }, function(err, existingUser) {
+      if (existingUser) return done(null, existingUser);
+      User.findOne({ email: profile._json.email }, function(err, existingEmailUser) {
+        if (existingEmailUser) {
+          req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Google manually from Account Settings.' });
+          done(err);
+        } else {
+          var user = new User();
+          user.email = profile._json.email;
+          user.google = profile.id;
+          user.tokens.push({ kind: 'google', accessToken: accessToken });
+          user.profile.name = profile.displayName;
+          user.profile.gender = profile._json.gender;
+          user.profile.picture = profile._json.picture;
+          user.save(function(err) {
+            done(err, user);
+          });
+        }
+      });
+    });
+  }
+}));
+
+
+
+var MongoStore = require('connect-mongo')(session);
+var mongoose = require('mongoose');
+var flash = require('express-flash');
 
 var routes = require('./routes');
 var user = require('./routes/user');
@@ -46,6 +106,9 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
+
+
 app.use(function(req, res, next) {
   // Make user object available in templates.
   res.locals.user = req.user;
@@ -64,6 +127,10 @@ app.use(function(req, res, next) {
 app.use(require('less-middleware')(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
+app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
+  res.redirect(req.session.returnTo || '/');
+});
 
 app.get('/', routes.index);
 app.get('/users', user.list);
