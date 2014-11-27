f468d7fc6ddd8b5fddd14c72351bfe6ba56b522a
Jakub Wasilewski <jakub.wasilewski@schibsted.pl>
2014-11-27 11:10:32 +0100

    Working camera.

--- app.js
+++ app.js
@@ -149,12 +149,7 @@ app.use(errorHandler());
 */
 
 app.listen(app.get('port'), function() {
-  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
-
-
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
 
 module.exports = app;
