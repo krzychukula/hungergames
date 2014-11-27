b3ecb0857a6793009b2ed7baad0432807f5f853a
Krzysztof Kula <krzychukula@gmail.com>
2014-11-27 10:09:31 +0100

    express 4

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
+var passport = require('passport');
 
 var routes = require('./routes');
 var user = require('./routes/user');
@@ -22,22 +23,40 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+//app.use(express.favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(expressValidator());
+app.use(methodOverride());
+app.use(cookieParser());
+// app.use(session({
+//   resave: true,
+//   saveUninitialized: true,
+//   secret: secrets.sessionSecret,
+//   store: new MongoStore({ url: secrets.db, auto_reconnect: true })
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(require('less-middleware')(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+/**
+* 500 Error Handler.
+*/
+
+app.use(errorHandler());
+
+/**
+* Start Express server.
+*/
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
+
+module.exports = app;
